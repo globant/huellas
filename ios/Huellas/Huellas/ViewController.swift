4b3c4f5da2b2aa69601c9d79e0be7909d6ac32b8
maxicasal <maximiliano.casal@globant.com>
2015-04-02 12:38:12 -0300

    LaunchScreen and Login UI

--- ios/Huellas/Huellas/ViewController.swift
+++ ios/Huellas/Huellas/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let transparent = UIColor.clearColor().CGColor
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var userTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.setUpUserTextField()
+        self.setUpPasswordTextField()
     }
+    func setUpUserTextField() {
 
+        let attriString = NSAttributedString(string:"Usuario", attributes:
+            [NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+     
+        self.userTextField.backgroundColor = UIColor.clearColor()
+        self.userTextField.attributedPlaceholder = attriString
+        self.userTextField.layer.borderColor = transparent
+    }
+    
+    func setUpPasswordTextField() {
+        let attriString = NSAttributedString(string:"Contraseña", attributes:
+            [NSForegroundColorAttributeName: UIColor.lightGrayColor()])
+        
+        self.passwordTextField.backgroundColor = UIColor.clearColor()
+        self.passwordTextField.attributedPlaceholder = attriString
+        self.passwordTextField.layer.borderColor = transparent
+    }
+    
+    @IBAction func onTapGesture(sender: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
