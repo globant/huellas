6a99795843c85b07f3a8142cc6c78f7ee3aa3473
gapoj <guillermoapoj@glb-d2791.local>
2015-04-07 13:26:22 -0300

    sidebar menu finished

--- ios/Huellas/Huellas/ViewController.swift
+++ ios/Huellas/Huellas/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBAction func act(sender: AnyObject) {
+        let storyboard = UIStoryboard(name: "Home", bundle: nil)
+        storyboard.instantiateInitialViewController()
+        let fvc = storyboard.instantiateViewControllerWithIdentifier("inicioViewController") as InicioViewController
+        let rvc = storyboard.instantiateViewControllerWithIdentifier("menuController") as MenuController
+        
+        let nfvc:UINavigationController = UINavigationController(rootViewController: fvc)
+        let nrvc:UINavigationController = UINavigationController(rootViewController: rvc)
+        let reveal:SWRevealViewController = storyboard.instantiateViewControllerWithIdentifier("revealViewController") as SWRevealViewController
+        reveal.rearViewController = nrvc
+        reveal.frontViewController = nfvc
+        self.presentViewController(reveal, animated: true, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+   
+     
     }
 
     override func didReceiveMemoryWarning() {
